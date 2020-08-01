b4f35960a80831f33c59e83d20fb2fe680aa1765
Mouse <mouse0w0@qq.com>
2020-08-01 23:19:56 +0800

    Publish to jcenter

--- src/main/java/com/github/mouse0w0/asm/extree/TypeAnnotationNodeEx.java
+++ src/main/java/com/github/mouse0w0/asm/extree/TypeAnnotationNodeEx.java
@@ -19,13 +19,14 @@ public class TypeAnnotationNodeEx extends AnnotationNodeEx {
 
     /**
      * Constructs a new {@link AnnotationNodeEx}. <i>Subclasses must not use this constructor</i>.
-     * Instead, they must use the {@link TypeAnnotationNodeEx(int, int, TypePath, String)} version.
+     * Instead, they must use the {@link TypeAnnotationNodeEx(int, int, TypePath, String, boolean)} version.
      *
      * @param typeRef    a reference to the annotated type. See {@link org.objectweb.asm.TypeReference}.
      * @param typePath   the path to the annotated type argument, wildcard bound, array element type, or
      *                   static inner type within 'typeRef'. May be {@literal null} if the annotation targets
      *                   'typeRef' as a whole.
      * @param descriptor the class descriptor of the annotation class.
+     * @param visible    the visibility of the annotation.
      * @throws IllegalStateException If a subclass calls this constructor.
      */
     public TypeAnnotationNodeEx(final int typeRef, final TypePath typePath, final String descriptor, final boolean visible) {
@@ -43,6 +44,7 @@ public class TypeAnnotationNodeEx extends AnnotationNodeEx {
      *                   static inner type within 'typeRef'. May be {@literal null} if the annotation targets
      *                   'typeRef' as a whole.
      * @param descriptor the class descriptor of the annotation class.
+     * @param visible the visibility of the annotation.
      */
     public TypeAnnotationNodeEx(
             final int api, final int typeRef, final TypePath typePath, final String descriptor, final boolean visible) {
